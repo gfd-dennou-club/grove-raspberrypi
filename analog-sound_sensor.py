434e7cf3f7523b7425ba105b5ff64d6629ab5e05
Ko-ichiro Sugiyama <32918854+sugiymki@users.noreply.github.com>
2022-11-19 18:37:28 +0900

    first commit

---
+++ analog-sound_sensor.py
@@ -0,0 +1,11 @@
+import time
+from grove.grove_sound_sensor import GroveSoundSensor
+
+PIN = 2
+
+sensor = GroveSoundSensor(PIN)
+
+print('Detecting sound...')
+while True:
+    print('Sound value: {0}'.format(sensor.sound))
+    time.sleep(.3)
